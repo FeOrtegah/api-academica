8f2e3c65ede34f992e63d7e92ac0c9a06b8e63a7
TheAntonio <ant.cornejom@duocuc.cl>
2026-06-14 22:46:22 -0400

    Update EvaluacionServiceTest.java

--- src/test/java/academica/academicas/services/EvaluacionServiceTest.java
+++ src/test/java/academica/academicas/services/EvaluacionServiceTest.java
@@ -26,18 +26,14 @@ class EvaluacionServiceTest {
 
     @Test
     void listarPorAsignaturaId_DeberiaRetornarLista() {
-        // Arrange
         Long asignaturaId = 10L;
         Evaluacion eval1 = new Evaluacion(1L, "Quiz", 0.2, LocalDate.now(), asignaturaId);
         Evaluacion eval2 = new Evaluacion(2L, "Parcial", 0.4, LocalDate.now(), asignaturaId);
         
         when(evaluacionRepository.findByAsignaturaId(asignaturaId))
             .thenReturn(Arrays.asList(eval1, eval2));
-
-        // Act
         List<Evaluacion> resultado = evaluacionService.listarPorAsignaturaId(asignaturaId);
 
-        // Assert
         assertThat(resultado).hasSize(2);
         assertThat(resultado.get(0).getAsignaturaId()).isEqualTo(asignaturaId);
         verify(evaluacionRepository, times(1)).findByAsignaturaId(asignaturaId);
@@ -45,10 +41,8 @@ class EvaluacionServiceTest {
 
     @Test
     void eliminarEvaluacion_DeberiaLlamarAlRepositorio() {
-        // Act
         evaluacionService.eliminarEvaluacion(5L);
 
-        // Assert
         verify(evaluacionRepository, times(1)).deleteById(5L);
     }
 }
